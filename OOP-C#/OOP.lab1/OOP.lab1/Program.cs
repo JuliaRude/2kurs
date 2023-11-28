b5e741f854db9ea0d03858dc744d71b19c7d3b34
JuliaRude <152305541+JuliaRude@users.noreply.github.com>
2023-11-28 17:58:18 +0200

    Add files via upload

---
+++ OOP-C#/OOP.lab1/OOP.lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+
+
+        Console.WriteLine("Введіть значення 'a':");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть значення 'b':");
+        double b = double.Parse(Console.ReadLine());
+
+        double q = Math.Pow((a + b), 5);
+        double w = Math.Pow((b - a), 3);
+
+        if (w == 0)
+        {
+            Console.WriteLine("Дільник дорівнює нулю. Неможливо обчислити корінь.");
+        }
+        else
+        {
+            double y = Math.Sqrt(q / w);
+            Console.WriteLine($"Результат: y = {y}");
+        }
+    }
+}
